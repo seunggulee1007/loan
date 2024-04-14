2d18d7ff5753ea4e140aade48195f197a81c5291
이승구 <leesg107@naver.com>
2024-04-14 11:28:19 +0900

    feat: 대출 도메인 작성

--- src/main/java/com/growthgenius/loan/domain/BaseEntity.java
+++ src/main/java/com/growthgenius/loan/domain/BaseEntity.java
@@ -1,2 +1,38 @@
-package com.growthgenius.loan.domain;public class BaseEntity {
+package com.growthgenius.loan.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+
+    /**
+     * 생성일자
+     */
+    @CreatedDate
+    @Column(updatable = false, columnDefinition = "datetime default CURRENT_TIMESTAMP NOT NULL COMMENT '생성일자'")
+    protected LocalDateTime createdAt;
+
+    /**
+     * 이용가능여부
+     */
+    @LastModifiedDate
+    @Column(columnDefinition = "datetime default CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL COMMENT '수정일자'")
+    protected LocalDateTime updatedAt;
+
+    /**
+     * 이용 가능 여부
+     */
+    @Column(name = "is_deleted")
+    protected boolean deleted;
+
 }
--- src/main/java/com/growthgenius/loan/domain/Counsel.java
+++ src/main/java/com/growthgenius/loan/domain/Counsel.java
@@ -1,2 +1,63 @@
-package com.growthgenius.loan.domain;public class Counsel {
+package com.growthgenius.loan.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Entity
+@Table(name = "counsel")
+public class Counsel extends BaseEntity {
+
+    @Id
+    @Column(name = "counsel_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    /**
+     * 주소
+     */
+    @Column(length = 50)
+    private String address;
+
+    /**
+     * 상세 주소
+     */
+    @Column(length = 50)
+    private String addressDetail;
+
+    /**
+     * 신청일자
+     */
+    private LocalDateTime appliedAt;
+
+    /**
+     * 전화번호
+     */
+    @Column(length = 13)
+    private String cellPhone;
+
+    /**
+     * 책임자 이메일
+     */
+    @Column(length = 50)
+    private String email;
+
+    /**
+     * 상담메모
+     */
+    private String memo;
+
+    /**
+     * 상담 요청자
+     */
+    @Column(length = 12)
+    private String name;
+
+    /**
+     * 우편번호
+     */
+    private String zipCode;
+
 }
