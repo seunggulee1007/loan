6e9c3c285ef4abe455aa8d696ad3d9b82af959eb
이승구 <leesg107@naver.com>
2024-05-29 15:39:22 +0900

    feat: 대출 신청 조회 컨트롤러 작성

--- src/main/java/com/growthgenius/loan/controller/ApplicationController.java
+++ src/main/java/com/growthgenius/loan/controller/ApplicationController.java
@@ -4,10 +4,7 @@ import com.growthgenius.loan.dto.ApplicationDto;
 import com.growthgenius.loan.dto.ResponseDto;
 import com.growthgenius.loan.service.ApplicationService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import static com.growthgenius.loan.dto.ResponseDto.ok;
 
@@ -21,6 +18,11 @@ public class ApplicationController extends AbstractController {
     @PostMapping
     public ResponseDto<ApplicationDto.Response> create(@RequestBody ApplicationDto.Request request) {
         return ok(applicationService.create(request));
+    }
+
+    @GetMapping("/{applicationId}")
+    public ResponseDto<ApplicationDto.Response> get(@PathVariable Long applicationId) {
+        return ok(applicationService.get(applicationId));
     }
 
 }
